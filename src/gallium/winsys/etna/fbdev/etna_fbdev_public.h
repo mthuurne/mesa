32df9fa7d4727b60a76e41a7b941911fe7ec2dd7
Wladimir J. van der Laan <laanwj@gmail.com>
2013-07-05 20:11:26 +0200

    fbdev: double buffering / vsync

--- src/gallium/winsys/etna/fbdev/etna_fbdev_public.h
+++ src/gallium/winsys/etna/fbdev/etna_fbdev_public.h
@@ -6,7 +6,7 @@ struct fb_var_screeninfo;
 struct fb_fix_screeninfo;
 
 /* Create etna fbdev drawable */
-struct etna_fbdev_drawable *etna_fbdev_drawable_create(void);
+struct etna_fbdev_drawable *etna_fbdev_drawable_create(struct pipe_screen *screen, bool want_fence);
 
 /* Destroy drawable */
 void etna_fbdev_drawable_destroy(struct etna_fbdev_drawable *drawable);
@@ -14,9 +14,14 @@ void etna_fbdev_drawable_destroy(struct etna_fbdev_drawable *drawable);
 /* Update dimensions and render target if needed */
 bool etna_fbdev_drawable_update(struct etna_fbdev_drawable *drawable,
                               const struct fb_var_screeninfo *vinfo,
-                              const struct fb_fix_screeninfo *finfo);
+                              const struct fb_fix_screeninfo *finfo,
+                              unsigned xoffset, unsigned yoffset,
+                              unsigned width, unsigned height);
 
 /* Get handle for passing to flush_frontbuffer */
-void *etna_fbdev_get_front_buffer(struct etna_fbdev_drawable *drawable);
+void *etna_fbdev_get_buffer(struct etna_fbdev_drawable *drawable);
+
+/* Get fence handle for frame, to know when to swap buffers */
+struct pipe_fence_handle *etna_fbdev_get_fence(struct etna_fbdev_drawable *drawable);
 
 #endif
